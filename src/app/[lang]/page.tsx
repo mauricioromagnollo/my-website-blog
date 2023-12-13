35b06a14617f9119b70439d7086030728c6f545f
mauricioromagnollo <mauricioromagnollo@gmail.com>
2023-12-13 01:05:53 -0300

    refactor: remove main tag from layout and fix font import

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -7,6 +7,8 @@ import { getDictionary } from "@/lib"
 
 import { PrismicApi } from "@/service"
 import { SocialButton, Text } from "@/ui"
+import { getHowManyYearsHavePassed } from "@/helpers"
+import { Months } from "@/constants"
 
 type HomeProps = {
   params: {
@@ -18,35 +20,45 @@ export default async function Home({ params: { lang } }: HomeProps) {
   const dictionary = await getDictionary(lang)
 
   const api = new PrismicApi()
-  const contact = await api.getContact()
+
+  const contact = await api.getContact({ lang })
+  const career = await api.getCareer({ lang })
 
   return (
-    <section className="flex items-center justify-center flex-col mt-4 px-4 md:px-0">
+    <main className="flex items-center justify-center flex-col mt-4 px-4 md:px-0">
       <PageUnderConstruction
         message={dictionary.under_construction_message}
       />
 
-      <div className="max-w-container">
+      <div className="max-w-container w-full">
 
         <div className={twMerge(
-          "p-4 rounded-lg shadow-md",
+          "p-2 rounded-lg shadow-md",
           'dark:border dark:border-primary-light',
           'dark:shadow-none'
         )}>
           <Text
             variant="h2"
-            className="text-center text-lg"
+            className="text-center text-md md:text-lg"
           >
             {dictionary.home.hello}
           </Text>
         </div>
 
-        <div className="flex flex-col-reverse md:flex-row items-center justify-center gap-10 mt-8">
-          <div>
+        <div className={twMerge(
+          'flex flex-col-reverse items-center justify-center mt-4',
+          'md:mt-8 md:gap-10 md:justify-between md:flex-row'
+        )}>
+          <div className="text-center md:text-left">
             <Text variant="h1">Maurício Romagnollo</Text>
-            <Text variant="h4" className="dark:text-primary-light mt-1">SOFTWARE ENGINEER</Text>
+            <Text variant="h4" className="dark:text-primary-light mt-1">
+              {career.currentPosition}
+            </Text>
+            <Text className="text-center md:text-left">
+              {`${getHowManyYearsHavePassed(new Date(2021, Months.May, 1))} ${dictionary.home.years_of_experience}`}
+            </Text>
             <Text className="mt-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. In porro eos adipisci neque iure culpa, numquam ducimus architecto corrupti ipsa eius, odio minus ex? Voluptatum eveniet numquam excepturi animi ratione.
+              {career.history}
             </Text>
           </div>
 
@@ -55,7 +67,11 @@ export default async function Home({ params: { lang } }: HomeProps) {
             alt="Profile Image"
             width={200}
             height={200}
-            className="rounded-full w-36 border-2 border-primary-dark dark:border-primary-light shadow-sm"
+            className={twMerge(
+              "rounded-full w-36 border-2 border-primary-dark shadow-sm",
+              'md:w-48',
+              'dark:border dark:border-primary-light',
+            )}
           />
         </div>
 
@@ -69,6 +85,6 @@ export default async function Home({ params: { lang } }: HomeProps) {
 
         <Text className="flex justify-center mt-4">{contact.email}</Text>
       </div>
-    </section>
+    </main>
   )
 }
