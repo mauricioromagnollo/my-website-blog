5391918c7c6c298b5e31da6c3603f16e6bc2a037
mauricioromagnollo <mauricioromagnollo@gmail.com>
2024-04-15 17:18:01 -0300

    refactor: remove hello message from home

--- src/components/page-under-construction.tsx
+++ src/components/page-under-construction.tsx
@@ -1,4 +1,5 @@
 import { ConstructionIcon, Text } from "@/ui"
+import { twMerge } from "tailwind-merge"
 
 type PageUnderConstructionProps = {
   message: string
@@ -6,9 +7,15 @@ type PageUnderConstructionProps = {
 
 export function PageUnderConstruction({ message }: PageUnderConstructionProps) {
   return (
-    <div className="flex items-center justify-center gap-4 my-4">
-      <ConstructionIcon />
-      <Text variant="h4">{message}</Text>
+    <div className={twMerge(
+      "rounded-lg shadow-md border border-accent-dark/30",
+      'dark:border dark:border-primary-light',
+      'dark:shadow-none'
+    )}>
+      <div className="flex items-center justify-center gap-4 my-2">
+        <ConstructionIcon />
+        <Text variant="h4">{message}</Text>
+      </div>
     </div>
   )
 }
